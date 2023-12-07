7d47b8ef5062dffd6d15c2cd65578fc1bdf314f8
Mohammad Amin Arjang <m-arjang@kms.net>
2023-12-07 09:33:36 +0330

    add account model

---
+++ Jabeja/Models/Account.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jabeja.Models
+{
+    [Index(nameof(Account.Username),IsUnique = true)]
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "نام")]
+        [Required(ErrorMessage ="لطفا {0} را وارد کنید")]
+        public string Name { get; set; }
+        [Display(Name = "آدرس ایمیل")]
+        [EmailAddress]
+        [Required(ErrorMessage ="لطفا {0} را وارد کنید")]
+        public string Email { get; set; }
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage ="لطفا {0} را وارد کنید")]
+        public string Password { get; set; }
+        [Display(Name="نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Username { get; set; }
+        [Display(Name= "شماره تلفن")]
+        [Required(ErrorMessage ="لطفا {0} را وارد کنید")]
+        public string? PhoneNumber { get; set; }
+        [Display(Name = "تاریخ ساخت")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public DateTime? CreatedDate { get; set; }
+
+        [Display(Name="وضعیت اکانت")]
+        [Required(ErrorMessage ="لطفا {0} را وارد کنید")]
+        public bool IsActive { get; set; }
+        public Account()
+        {
+            
+        }
+    }
+}
