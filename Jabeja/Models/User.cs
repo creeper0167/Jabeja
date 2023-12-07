7af0b86a790d72ee00ecdc50d14460819c08fd63
Mohammad Amin Arjang <m-arjang@kms.net>
2023-12-07 13:09:09 +0330

    add user model

---
+++ Jabeja/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jabeja.Models
+{
+    [Index(nameof(User.Username), IsUnique = true)]
+    public class User : Account
+    {
+        
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Username { get; set; }
+        public double? Stars { get; set; }
+
+        public User()
+        {
+            
+        }
+
+    }
+}
